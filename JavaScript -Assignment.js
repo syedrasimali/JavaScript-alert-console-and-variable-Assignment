21f36f073dbb5e9f9fd9aea0618ecadc071b7341
Syed Rasim Ali <alisyedrasim@gmail.com>
2025-08-31 04:21:58 -0700

    Add files via upload

---
+++ JavaScript -Assignment.js
@@ -0,0 +1,35 @@
+//1. Alerts
+//Alerts are popup boxes.
+//Example:
+
+alert("Welcome to JavaScript!");
+
+/*Assignment:
+Show an alert that says:
+"Hello World!"
+Show another alert that says:
+"I am learning JavaScript"*/
+
+/*2. Console.log
+console.log() shows output in the browser console (press F12 → Console).
+Example:*/
+
+console.log("This is console output.");
+
+//Assignment:
+//this is output of console "This is console output"
+
+
+/*3. Variables (var)
+Variables store data.
+Example:*/
+
+var myName = "Ali";
+alert("My name is " + myName);
+console.log("My name is " + myName);
+
+/*Assignment:
+Create a variable called myName and store your name in it.
+Show it using alert.
+Show it again using console.log.*/
+
